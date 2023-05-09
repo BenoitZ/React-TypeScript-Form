284f413cb248d4733cdd0c81dbd00a5ef38eb2b0
BenoitZ <103201206+BenoitZ@users.noreply.github.com>
2023-05-09 09:41:42 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from 'react';
+import { MessageInt } from './model';
+import Message from './component/Message';
+
+const App = () => {
+const inputMessage = useRef<HTMLInputElement>(null)
+const [messData, setMessData] = useState<MessageInt[]>([])
+
+
+const handleSubmit = (e:any) => {
+  e.preventDefault();
+
+if(inputMessage){
+  const mess:MessageInt = {
+    id: Math.round(Math.random() * Date.now()),
+    message: inputMessage?.current?.value,
+    date : Date.now()
+  }
+  setMessData((prevData) => [...prevData, mess]);
+}
+  (document.getElementById("inputMessage") as HTMLInputElement).value = "";
+
+}
+
+  return (
+    <div>
+      <h2>Poster un message</h2>
+      <form onSubmit = {(e) =>{handleSubmit(e)}}>
+        <input type="text" placeholder='Entrez un message' id="inputMessage" ref={inputMessage} />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <h2>Liste des messages</h2>
+      <div>
+        {messData?.map((mess) => (
+          <Message mess={mess} messData={messData} setMessData={setMessData} key={mess.id}/>
+        ))};
+      </div>
+    </div>
+  );
+};
+
+export default App;
